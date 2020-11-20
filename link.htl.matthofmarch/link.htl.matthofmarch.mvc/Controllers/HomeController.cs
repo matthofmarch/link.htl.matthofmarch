a4aada81db5c035d8bd34e4f8dd0b4015a4da8bd
Matthias Hofmarcher <hofmarchermatthias@gmail.com>
2020-11-20 20:30:34 +0100

    first commit

---
+++ link.htl.matthofmarch/link.htl.matthofmarch.mvc/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using link.htl.matthofmarch.mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace link.htl.matthofmarch.mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private static readonly List<Guest> _guests = new List<Guest>();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+
+        }
+
+        public IActionResult Index()
+        {
+            return View(_guests);
+        }
+
+        [HttpGet] 
+        public IActionResult SignUp()
+        {
+           return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(Guest viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            _guests.Add(viewModel??new Guest{Name= "Invalid Username" });
+
+            return RedirectToAction("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ link.htl.matthofmarch/link.htl.matthofmarch.mvc/Models/Guest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace link.htl.matthofmarch.mvc.Models
+{
+    public class Guest
+    {
+        public string Name { get; set; }
+    }
+}

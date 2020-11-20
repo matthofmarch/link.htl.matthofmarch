32f0db42046e8f60d033c4fa872f53996873f4f8
Matthias Hofmarcher <hofmarchermatthias@gmail.com>
2020-11-20 22:23:27 +0100

    Forwarding

--- link.htl.matthofmarch/link.htl.matthofmarch.mvc/Startup.cs
+++ link.htl.matthofmarch/link.htl.matthofmarch.mvc/Startup.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.HttpOverrides;
 
 namespace link.htl.matthofmarch.mvc
 {
@@ -39,11 +40,14 @@ namespace link.htl.matthofmarch.mvc
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
